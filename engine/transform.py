4f1a818f60107a6dbfc0b046b5cadf40c4b400ea
Sebastian Vizcay <sebastian.vizcay@usach.cl>
2021-12-09 19:46:28 +0100

    adding quaternions

--- engine/transform.py
+++ engine/transform.py
@@ -56,33 +56,133 @@ class Transform:
     # whenever those values are set, we dirty the matrices.
     # the renderers read those matrices and if the values are dirtied,
     # they are recalculated
-    def __init__(self, game_object = None, pos = None, rotation = None, scale = None):
+    def __init__(self, game_object = None, pos = None, euler_angles = None, scale = None):
         # a tranform can not exists without a gameObject
         print("creating transform for gameObject={}".format(game_object))
         self.game_object = game_object
-        # 'raw' values which are expected to be set by clients
-        # we are going to use properties so we can update the internal matrices accordingly
-        # most pyrr functions work on pyrr.VectorX objects.
-        # and pyrr.Vector3 is constructed from a python array.
+
+        # internal data
         self._position = pos if pos is not None else pyrr.Vector3([0, 0, 0])
-        self._rotation = rotation if rotation is not None else pyrr.Vector3([0, 0, 0])
+
+        self._euler_angles = euler_angles if euler_angles is not None else pyrr.Vector3([0, 0, 0])
+        # for each euler angle:
+        # if the euler angle is zero -> whatever is the direction we use, the quaternion is (1, 0, 0, 0)
+        self.rotation = Transform.quaternion_from_euler(self._euler_angles) # identity quaternion
         self._scale = scale if scale is not None else pyrr.Vector3([1, 1, 1])
 
-        # let's also provide shortcuts for
-        # right, up, forward.
-        # should we allow setting them?
-        # i dont think so, better to provide lookAt method that set thems all together
-        self.right = pyrr.Vector3([1, 0, 0])
-        self.up = pyrr.Vector3([0, 1, 0])
-        self.forward = pyrr.Vector3([0, 0, 1])
-
-
+        # internal matrices
         self._translation_dirty = True
         self._rotation_dirty = True
         self._scale_dirty = True
 
         self._model_dirty = True
         self._view_dirty = True
+
+        # directions (they should be extracted from the model matrix
+        # self.right = pyrr.Vector3([1, 0, 0])
+        # self.up = pyrr.Vector3([0, 1, 0])
+        # self.forward = pyrr.Vector3([0, 0, 1])
+
+        ### # test for generating matrices and quaternion form euler angles -> success
+        print("**********************")
+        print("testing rotation matrix from quaternion")
+        ### # euler_rotation = [0, 90, 0] # 90 degrees yaw direction (lateral view)
+        euler_rotation = [10, 20, 30] # 90 degrees yaw direction (lateral view)
+        print("euler = {}".format(euler_rotation))
+        quaternion = Transform.quaternion_from_euler(euler_rotation)
+        rotation_matrix = Transform.matrix_from_quaternion(quaternion)
+        resulting_euler = Transform.euler_from_quaternion(quaternion)
+        print("euler back = {}".format(resulting_euler))
+        exit()
+        ### # testing values
+        ### print("rotation matrix")
+        print(rotation_matrix)
+        ### print("rotating x")
+        ### print(pyrr.matrix44.apply_to_vector(rotation_matrix, pyrr.Vector3([1, 0, 0])))
+        ### print("rotating y")
+        ### print(pyrr.matrix44.apply_to_vector(rotation_matrix, pyrr.Vector3([0, 1, 0])))
+        ### print("rotating z")
+        ### print(pyrr.matrix44.apply_to_vector(rotation_matrix, pyrr.Vector3([0, 0, 1])))
+        exit()
+
+        ### print("using only rotation matrices:")
+        ### roll = math.radians(euler_rotation[2])
+        ### pitch = math.radians(euler_rotation[0])
+        ### yaw = math.radians(euler_rotation[1])
+        ### individual_x = pyrr.matrix44.create_from_x_rotation(pitch)
+        ### individual_y = pyrr.matrix44.create_from_y_rotation(yaw)
+        ### individual_z = pyrr.matrix44.create_from_z_rotation(roll)
+        ### # transpose pyrr rotation matrices
+        ### individual_x = individual_x.T
+        ### individual_y = individual_y.T
+        ### individual_z = individual_z.T
+        ### rotation_matrix_old = pyrr.matrix44.multiply(
+        ###         pyrr.matrix44.multiply(individual_z, individual_y),
+        ###         individual_x)
+        ### print(rotation_matrix_old)
+        ### print("rotating x")
+        ### print(pyrr.matrix44.apply_to_vector(rotation_matrix_old, pyrr.Vector3([1, 0, 0])))
+        ### print("rotating y")
+        ### print(pyrr.matrix44.apply_to_vector(rotation_matrix_old, pyrr.Vector3([0, 1, 0])))
+        ### print("rotating z")
+        ### print(pyrr.matrix44.apply_to_vector(rotation_matrix_old, pyrr.Vector3([0, 0, 1])))
+        ### exit()
+        ### # end test -> sucess
+
+        # print("testing translation matrix numpy order")
+        # it seems numpy data is layered by rows
+        # if matrix =   | a b c |
+        #               | d e f |
+        #               | f g h |
+        # then, that would be created in numpy as:
+        # mat = np.array( [a,b,c], [d,e,f], [g,h,i] )
+        # and the linear iteration will also go for rows
+        # for idx, x in np.ndenumerate(arr):
+        # idx is a tuple where idx[0] is the row and idx[1] is the col (x is the actual value)
+        # np_translation_matrix = pyrr.matrix44.create_from_translation(pyrr.Vector3([1, 2, 3]))
+        # pyrr will put the translation in the last ROW of the numpy matrix
+        # which can be interpreted as well as a matrix stored by cols, i.e
+        # instead of considering the last element of the numpy matrix as a row,
+        # we can consider it as a col.
+        # the way pyrr does it is as follows:
+        # - create an identity 4x4 matrix
+        # - set mat[3, 0:3] = vec[:3]
+        # which also works for row major multiplication when data is considered to be layout
+        # by rows
+        # print(np_translation_matrix)
+        # for idx, x in np.ndenumerate(np_translation_matrix):
+        #     print("T[{},{}]={}".format(idx[0], idx[1], x))
+        # for i in range len(np_translation_matrix):
+        #     for j in range len(np_translation_matrix[i]):
+        #         print("T[{},{}]={}".format(i, j, np_translation_matrix[i, j]))
+        #     print("")
+        # print("")
+        # exit()
+
+        # 'raw' values which are expected to be set by clients
+        # we are going to use properties so we can update the internal matrices accordingly
+        # most pyrr functions work on pyrr.VectorX objects.
+        # and pyrr.Vector3 is constructed from a python array.
+
+        # self._rotation = rotation if rotation is not None else pyrr.Vector3([0, 0, 0])
+
+        # let's also provide shortcuts for
+        # right, up, forward.
+        # should we allow setting them?
+        # i dont think so, better to provide lookAt method that set thems all together
+
+        # the quaternion is going to be the actual internal data and not the euler angles
+        # we were calling until now 'rotation' to the vec3 representing the euler angles.
+        # testing our own quaternion
+        # angle (scalar), x,y,z (axis)
+        # the actual versor is constructed as follows (in radians):
+        # making a 'versor' is similar to making a rotation matrix based on some euler angles
+        # qo = cos(angle/2)
+        # q1 = sin(angle/2) * x
+        # q2 = sin(angle/2) * y
+        # q3 = sin(angle/2) * z
+        # the resulting versor needs to have norm=1
+
 
         # we are going to keep track of the following dirty matrices
         # translation, rotation, scale, model, view
@@ -132,6 +232,9 @@ class Transform:
         self._model_dirty = True
         self._view_dirty = True
 
+    # we were calling 'rotation' the euler angles
+    # should we allow this setter?
+    # yes, the 'inspector' can work based on these values
     @rotation.setter
     def rotation(self, value):
         self._rotation = value
@@ -178,6 +281,30 @@ class Transform:
             self._set_view_matrix()
         return self._view_mat
 
+    @property
+    def right(self):
+        if (self._model_dirty):
+            self._set_model_matrix()
+        # right is the first col of the model matrix
+        # and it's stored in numpy matrix as the first row (mat[0])
+        return self.model_mat[0,:3]
+
+    @property
+    def up(self):
+        if (self._model_dirty):
+            self._set_model_matrix()
+        # up is the second col of the model matrix
+        # and it's stored in numpy matrix as the second row (mat[1])
+        return self.model_mat[1,:3]
+
+    @property
+    def forward(self):
+        if (self._model_dirty):
+            self._set_model_matrix()
+        # up is the third col of the model matrix
+        # and it's stored in numpy matrix as the third row (mat[2])
+        return self.model_mat[2,:3]
+
     # utility to set the rotation matrix)
     def look_at(self, eye_pos, target, up):
         # local positive z
@@ -216,6 +343,8 @@ class Transform:
         self._translation_dirty = False
 
     def _set_rotation_matrix(self):
+        # for now this is called when we modified the euler angles
+
         # print("updating rotation matrix of {} dirty={}".format(self.game_object.name, self._rotation_dirty))
         # traceback.print_stack()
         # rot_x = pyrr.Matrix44.from_x_rotation(self._rotation[0])
@@ -356,4 +485,130 @@ class Transform:
 
     # we might want a method to get the inverse but done
     # by knowing m = t * r * s
+
+    # static method don't take class type as first parameter
+    # they don't modify the class at all compared to class methods
+    # which have access to class data.
+    @staticmethod
+    def quaternion_from_euler(euler):
+        # we need to create a rotation that is equivalent to rotate
+        # RxRyRz with values specified by euler in degrees
+        x_rad = math.radians(euler[0])
+        y_rad = math.radians(euler[1])
+        z_rad = math.radians(euler[2])
+
+        # we can create 3 quaternions representing this individual rotations.
+        # euler rotations are in world space so when constructing the quaternion,
+        # we also need to create them using as axis a world space axis
+        quaternion_x = Transform.versor_from_angle_axis(x_rad, [1, 0, 0])
+        quaternion_y = Transform.versor_from_angle_axis(y_rad, [0, 1, 0])
+        quaternion_z = Transform.versor_from_angle_axis(z_rad, [0, 0, 1])
+
+        # in what order we multiply them now?
+        # according to our book, to rotate by r first and them by q,
+        # we should calculate t = q * r
+        # i.e t = quaternion_multiply(q, r)
+        # the following works! Rx*Ry*Rz
+        rotation = Transform.quaternion_multiply(
+            quaternion_x,
+            Transform.quaternion_multiply(quaternion_y, quaternion_z)
+        )
+
+        # test:
+        # let's see if we can get the right euler using wikipedia formula
+        # if we use the specified order there: Ry*Rx*Rz (remember this is not pyrr!!)
+        # it didn't work
+        # rotation = Transform.quaternion_multiply(
+        #     quaternion_y,
+        #     Transform.quaternion_multiply(quaternion_x, quaternion_z)
+        # )
+        return rotation
+
+
+    # implementing wikipedia's version
+    # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Quaternion_to_Euler_angles_conversion
+    # we are going to return them in degrees.
+    # wikipedia formula was made for:
+    # yaw (body-z) turning onto the runway
+    # pitch (body-y) take off
+    # rolls (body-x) in the air
+    # but those seem to be local angles
+    # if they are local angles, it's because they were done in that order
+    # i.e product: Rv = Yaw * Pitch * Rolls * v
+    # which for those refer to:
+    # yaw -> Ry
+    # pitch -> Rx
+    # rolls -> Rz
+    # let's see if we get the right conversation back if we generate the quaternion following: Ry*Rx*Rz * v
+    @staticmethod
+    def euler_from_quaternion(quaternion):
+        q0 = quaternion[0]
+        q1 = quaternion[1]
+        q2 = quaternion[2]
+        q3 = quaternion[3]
+
+        # maybe we are forgetting normalize the quaternion
+        print("input quaternion: {}".format(quaternion))
+        length = math.sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3)
+        q0 = q0 / length
+        q1 = q1 / length
+        q2 = q2 / length
+        q3 = q3 / length
+        print("normalized quaternion: {}, {}, {}, {}".format(q0, q1, q2, q3))
+
+        a = np.arctan2(2 * (q0*q1 + q2*q3), 1 - 2 * (q1*q1 + q2*q2))
+        b = np.arcsin(2 * (q0*q2 - q3*q1))
+        c = np.arctan2(2 * (q0*q3 + q1*q2), 1 - 2 * (q2*q2 + q3*q3))
+
+        # trying using the formulas from here
+        # https://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/
+        a = np.arctan2(2 * (q0*q2 - q1*q3), 1 - 2 * (q2*q2 + q3*q3))
+        b = np.arcsin(2 * (q1*q2 + q3*q0))
+        c = np.arctan2(2 * (q0*q1 - q2*q3), 1 - 2 * (q1*q1 + q3*q3))
+
+        a = math.degrees(a)
+        b = math.degrees(b)
+        c = math.degrees(c)
+
+        return [a, b, c]
+
+
+
+    # axis must be already normalized
+    @staticmethod
+    def versor_from_angle_axis(angle, axis):
+        half_angle = angle/2.0
+        sin_half_angle = np.sin(half_angle)
+        q0 = np.cos(half_angle)
+        q1 = sin_half_angle * axis[0]
+        q2 = sin_half_angle * axis[1]
+        q3 = sin_half_angle * axis[2]
+        return (q0, q1, q2, q3)
+
+    # quaterion multiplication
+    @staticmethod
+    def quaternion_multiply(q, r):
+        t = [0, 0, 0, 0]
+        t[0] = r[0] * q[0] - r[1] * q[1] - r[2] * q[2] - r[3] * q[3]
+        t[1] = r[0] * q[1] + r[1] * q[0] - r[2] * q[3] + r[3] * q[2]
+        t[2] = r[0] * q[2] + r[1] * q[3] + r[2] * q[0] - r[3] * q[1]
+        t[3] = r[0] * q[3] - r[1] * q[2] + r[2] * q[1] + r[3] * q[0]
+        return t
+
+    @staticmethod
+    def matrix_from_quaternion(q):
+        # i need to fill up a numpy matrix.
+        # we will make it so it's ready to be used as col major multiplication and storage
+        w = q[0]
+        x = q[1]
+        y = q[2]
+        z = q[3]
+        m = np.identity(4)
+        m[0] = [(1 - 2*y*y - 2*z*z), (2*x*y - 2*w*z), (2*x*z + 2*w*y), 0]
+        m[1] = [(2*x*y + 2*w*z), (1 - 2*x*x - 2*z*z), (2*y*z - 2*w*x), 0]
+        m[2] = [(2*x*z - 2*w*y), (2*y*z + 2*w*x), (1 - 2*x*x - 2*y*y), 0]
+        m[3] = [0, 0, 0, 1]
+        return m.T
+
+
 
